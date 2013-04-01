f98ab7b14b9f1944258b6052757703292f82f288
refaktorco <refaktor.co@gmail.com>
2013-04-01 13:43:52 -0700

    -Pushing to check something.

--- templates/node--portfolio.tpl.php
+++ templates/node--portfolio.tpl.php
@@ -42,11 +42,13 @@
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
-      hide($content['links']);
+  
       hide($content['field_portfolio_image']);
       hide($content['field_tags']);
       print render($content);
     ?>
+    
+   
   </div>
 
   </div>
