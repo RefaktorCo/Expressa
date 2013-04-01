925bf8edcae90d01d53d0e51c92e9bb9ee861ba5
refaktorco <refaktor.co@gmail.com>
2013-04-01 12:13:45 -0700

    -Starting on initial theme options overhaul.

--- theme-settings.php
+++ theme-settings.php
@@ -1,10 +1,93 @@
 <?php
-
 /**
  * Implements hook_form_system_theme_settings_alter()
  */
 function expressa_form_system_theme_settings_alter(&$form, &$form_state) {
 
+   // Default path for image
+  $bg_path = theme_get_setting('bg_path');
+  if (file_uri_scheme($bg_path) == 'public') {
+    $bg_path = file_uri_target($bg_path);
+  }
+
+
+  // Main settings wrapper
+  $form['options'] = array(
+    '#type' => 'vertical_tabs',
+    '#default_tab' => 'defaults',
+    '#weight' => '-10',
+    '#attached' => array(
+      'css' => array(drupal_get_path('theme', 'expressa') . '/css/theme-settings.css'),
+    ),
+  );
+  
+  // General
+  $form['options']['general'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'General',
+  );
+  
+    // Breadcrumbs
+    $form['options']['general']['breadcrumbs'] = array(
+      '#type' => 'checkbox',
+      '#title' => 'Breadcrumbs',
+      '#default_value' => theme_get_setting('breadcrumbs'),
+    );
+        
+    // SEO
+    $form['options']['general']['seo'] = array(
+      '#type' => 'fieldset',
+      '#title' => '<div class="plus"></div><h3 class="options_heading">SEO</h3>',
+    );
+    
+      // SEO Title
+      $form['options']['general']['seo']['seo_title'] = array(
+        '#type' => 'textfield',
+        '#title' => 'Title',
+        '#default_value' => theme_get_setting('seo_title'),
+      );
+      
+       // SEO Description
+      $form['options']['general']['seo']['seo_description'] = array(
+        '#type' => 'textarea',
+        '#title' => 'Description',
+        '#default_value' => theme_get_setting('seo_description'),
+      );
+      
+       // SEO Keywords
+      $form['options']['general']['seo']['seo_keywords'] = array(
+        '#type' => 'textarea',
+        '#title' => 'Keywords',
+        '#default_value' => theme_get_setting('seo_keywords'),
+      );
+  
+  // Submit Button
+  $form['#submit'][] = 'blocks_settings_submit';
+
 }
+
+function expressa_settings_submit($form, &$form_state) {
+  // Get the previous value
+  $previous = 'public://' . $form['options']['header']['branding']['bg_path']['#default_value'] ;
+  
+  $file = file_save_upload('bg_upload');
+  if ($file) {
+    $parts = pathinfo($file->filename);
+    $destination = 'public://' . $parts['basename'];
+    $file->status = FILE_STATUS_PERMANENT;
+    
+    if(file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
+      $_POST['bg_path'] = $form_state['values']['bg_path'] = $destination;
+      if ($destination != $previous) {
+        return;
+      }
+    }
+  } else {
+    // Avoid error when the form is submitted without specifying a new image
+    $_POST['bg_path'] = $form_state['values']['bg_path'] = $previous;
+  }
+  
+}
+
 
 ?>
