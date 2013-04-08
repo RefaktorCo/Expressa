cf6dd8e021b14d640786fa582aec028efbd94cde
refaktorco <refaktor.co@gmail.com>
2013-04-08 14:54:06 -0700

    -View support almost finished

---
+++ templates/node--image-slider.tpl.php
@@ -0,0 +1,6 @@
+<li>
+  <?php print render($content['field_slider_image']); ?>
+  <div class="ei-title">
+	  <?php print render($content['field_slider_caption']); ?>     
+	</div>
+</li>
---
+++ templates/node--view--image-slider-thumbnails--block.tpl.php
@@ -0,0 +1,4 @@
+<li>
+  <a href="#">Slide 1</a>
+  <?php print render($content['field_slider_image']); ?>
+</li>
