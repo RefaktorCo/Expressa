f9440518b99326f2138b4b43bd962a2f12cff80b
refaktorco <refaktor.co@gmail.com>
2013-04-09 15:17:13 -0700

    -Finally figured out syntax to hide form elements based on select item.

--- theme-settings.php
+++ theme-settings.php
@@ -25,15 +25,9 @@ drupal_add_js(drupal_get_path('theme', 'expressa') .'/js/theme_settings.js');
   // General
   $form['options']['general'] = array(
     '#type' => 'fieldset',
-    '#title' => 'General',
+    '#title' => t('General'),
   );
-  
-    $form['options']['general']['test_color'] = array(
-		  '#type' => 'jquery_colorpicker',
-		  '#title' => t('Color'),
-		  '#default_value' => theme_get_setting('test_color'),
-    );
-        
+          
      $form['options']['general']['image_1'] = array(
 			  '#title' => t('Image'),
 			  '#type' => 'managed_file',
@@ -62,14 +56,104 @@ drupal_add_js(drupal_get_path('theme', 'expressa') .'/js/theme_settings.js');
   // Layout
   $form['options']['layout'] = array(
     '#type' => 'fieldset',
-    '#title' => 'Layout',
+    '#title' => t('Layout'),
   );  
   
+  // Color
+  $form['options']['color'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Color'),
+  );  
+  
+  // Colors
+    $form['options']['color']['colors'] = array(
+      '#type' => 'fieldset',
+      '#title' => '<div class="plus"></div><h3 class="options_heading">Color Scheme</h3>',
+    );
+  
+      // Color Scheme
+      $form['options']['color']['colors']['color_scheme'] = array(
+        '#type' => 'select',
+        '#title' => 'Color Scheme',
+        '#default_value' => theme_get_setting('color_scheme'),
+        '#options' => array(
+          'black' => t('Black'),
+          'blue' => t('Blue (default)'),
+          'teal' => t('Teal'),
+          'green' => t('Green'),
+          'yellow' => t('Yellow'),
+          'purple' => t('Purple'),
+          'orange' => t('Orange'),
+          'red' => t('Red'),
+          'custom' => t('custom'),
+        ),
+      );
+      
+      $form['options']['color']['colors']['custom_color'] = array(
+		    '#type' => 'jquery_colorpicker',
+		    '#title' => t('Color'),
+		    '#default_value' => theme_get_setting('custom_color'),
+		    '#states' => array (
+          'visible' => array(
+            'select[name=color_scheme]' => array('value' => 'custom')
+          )
+        )
+      );
+      
   // Typography
   $form['options']['typography'] = array(
     '#type' => 'fieldset',
     '#title' => 'Typography',
   ); 
+  
+    //Headings
+    $form['options']['typography']['headings'] = array(
+      '#type' => 'fieldset',
+      '#title' => '<div class="plus"></div><h3 class="options_heading">Headings</h3>',
+    );
+        
+      //H1
+      $form['options']['typography']['headings']['h1'] =array(
+        '#type' => 'textfield',
+        '#title' => t('h1 Size'),
+        '#default_value' => theme_get_setting('h1'),
+      );
+      
+      //H2
+      $form['options']['typography']['headings']['h2'] =array(
+        '#type' => 'textfield',
+        '#title' => t('h2 Size'),
+        '#default_value' => theme_get_setting('h2'),
+      );
+      
+      //H3
+      $form['options']['typography']['headings']['h3'] =array(
+        '#type' => 'textfield',
+        '#title' => t('h3 Size'),
+        '#default_value' => theme_get_setting('h3'),
+      );
+      
+      //H4
+      $form['options']['typography']['headings']['h4'] =array(
+        '#type' => 'textfield',
+        '#title' => t('h4 Size'),
+        '#default_value' => theme_get_setting('h4'),
+      );
+      
+      //H5
+      $form['options']['typography']['headings']['h5'] =array(
+        '#type' => 'textfield',
+        '#title' => t('h5 Size'),
+        '#default_value' => theme_get_setting('h5'),
+      );
+      
+      //H6
+      $form['options']['typography']['headings']['h6'] =array(
+        '#type' => 'textfield',
+        '#title' => t('h6 Size'),
+        '#default_value' => theme_get_setting('h6'),
+      );
+
   
   // SEO
   $form['options']['seo'] = array(
@@ -111,35 +195,6 @@ drupal_add_js(drupal_get_path('theme', 'expressa') .'/js/theme_settings.js');
       '#description' => t('Add any tracking code such as Google Analytics.'),
       '#default_value' => theme_get_setting('tracking_code'),
     );
-
-  
-  // Submit Button
-  // $form['#submit'][] = 'blocks_settings_submit';
-
 }
-
-function expressa_settings_submit($form, &$form_state) {
-  // Get the previous value
-  $previous = 'public://' . $form['options']['header']['branding']['bg_path']['#default_value'] ;
-  
-  $file = file_save_upload('bg_upload');
-  if ($file) {
-    $parts = pathinfo($file->filename);
-    $destination = 'public://' . $parts['basename'];
-    $file->status = FILE_STATUS_PERMANENT;
-    
-    if(file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
-      $_POST['bg_path'] = $form_state['values']['bg_path'] = $destination;
-      if ($destination != $previous) {
-        return;
-      }
-    }
-  } else {
-    // Avoid error when the form is submitted without specifying a new image
-    $_POST['bg_path'] = $form_state['values']['bg_path'] = $previous;
-  }
-  
-}
-
 
 ?>
