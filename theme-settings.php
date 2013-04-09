4c726ad228e37a7d71d8d01d06ab012532038377
refaktorco <refaktor.co@gmail.com>
2013-04-09 14:12:27 -0700

    -Working on theme settings.

--- theme-settings.php
+++ theme-settings.php
@@ -28,65 +28,90 @@ drupal_add_js(drupal_get_path('theme', 'expressa') .'/js/theme_settings.js');
     '#title' => 'General',
   );
   
-  
     $form['options']['general']['test_color'] = array(
 		  '#type' => 'jquery_colorpicker',
-		  '#title' => 'Color',
+		  '#title' => t('Color'),
 		  '#default_value' => theme_get_setting('test_color'),
     );
         
      $form['options']['general']['image_1'] = array(
 			  '#title' => t('Image'),
 			  '#type' => 'managed_file',
-			  '#description' => t('The uploaded image will be displayed on this page using the image style choosen below.'),
+			  '#description' => t('The uploaded image will be displayed on this page using the image style chosen below.'),
 			  '#default_value' => theme_get_setting('image_1'),
 			  '#upload_location' => 'public://image_example_images/',
 			  '#upload_validators' => array(
 	        'file_validate_extensions' => array('gif png jpg jpeg'),
 	      ),
       );
- 
   
     // Breadcrumbs
     $form['options']['general']['breadcrumbs'] = array(
       '#type' => 'checkbox',
-      '#title' => 'Breadcrumbs',
+      '#title' => t('Breadcrumbs'),
       '#default_value' => theme_get_setting('breadcrumbs'),
     );
     
     // Scroll Menu
     $form['options']['general']['scroll_menu'] = array(
       '#type' => 'checkbox',
-      '#title' => 'Scroll Menu',
+      '#title' => t('Scroll Menu'),
       '#default_value' => theme_get_setting('scroll_menu'),
     );
-        
-    // SEO
-    $form['options']['general']['seo'] = array(
-      '#type' => 'fieldset',
-      '#title' => '<div class="plus"></div><h3 class="options_heading">SEO</h3>',
+                  
+  // Layout
+  $form['options']['layout'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'Layout',
+  );  
+  
+  // Typography
+  $form['options']['typography'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'Typography',
+  ); 
+  
+  // SEO
+  $form['options']['seo'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'SEO',
+  ); 
+  
+    // SEO Title
+    $form['options']['seo']['seo_title'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Title'),
+      '#default_value' => theme_get_setting('seo_title'),
     );
     
-      // SEO Title
-      $form['options']['general']['seo']['seo_title'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Title',
-        '#default_value' => theme_get_setting('seo_title'),
-      );
-      
-       // SEO Description
-      $form['options']['general']['seo']['seo_description'] = array(
-        '#type' => 'textarea',
-        '#title' => 'Description',
-        '#default_value' => theme_get_setting('seo_description'),
-      );
-      
-       // SEO Keywords
-      $form['options']['general']['seo']['seo_keywords'] = array(
-        '#type' => 'textarea',
-        '#title' => 'Keywords',
-        '#default_value' => theme_get_setting('seo_keywords'),
-      );
+     // SEO Description
+    $form['options']['seo']['seo_description'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Description'),
+      '#default_value' => theme_get_setting('seo_description'),
+    );
+    
+     // SEO Keywords
+    $form['options']['seo']['seo_keywords'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Keywords'),
+      '#default_value' => theme_get_setting('seo_keywords'),
+    );
+  
+   // Tracking
+  $form['options']['tracking'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Tracking'),
+  ); 
+  
+    // Tracking Code
+    $form['options']['tracking']['tracking_code'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Tracking Code'),
+      '#description' => t('Add any tracking code such as Google Analytics.'),
+      '#default_value' => theme_get_setting('tracking_code'),
+    );
+
   
   // Submit Button
   // $form['#submit'][] = 'blocks_settings_submit';
