0c626b4efe20cc9b9f6bec1994d0a8545cb30069
refaktorco <refaktor.co@gmail.com>
2013-04-11 18:09:38 -0700

    -New css and templates for new facet search functionality.

--- templates/node--product-display.tpl.php
+++ templates/node--product-display.tpl.php
@@ -1,4 +1,5 @@
-<div class="span3">
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
   <a href="<?php print $node_url; ?>"><?php echo $title; ?></a>
 
   <?php print render($content['product:field_image']); ?>
@@ -7,4 +8,4 @@
   
   <?php print render($content['field_reference']); ?>
 
-</div>
+</article>
