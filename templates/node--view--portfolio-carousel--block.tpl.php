33e48734e020b0ea635dac53a43a833ab5fcb274
refaktorco <refaktor.co@gmail.com>
2013-04-23 09:05:06 -0700

    -Portfolio carousel stuff, going to clean up div classes and CSS.

--- templates/node--view--portfolio-carousel--block.tpl.php
+++ templates/node--view--portfolio-carousel--block.tpl.php
@@ -3,8 +3,10 @@
     <figure>
       <a class="enlarge" href="<?php echo file_create_url($node->field_portfolio_image['und'][0]['uri']); ?>" rel="prettyPhoto" title="<?php print $title; ?>"><?php print render($content['field_portfolio_image']); ?></a>
     </figure>
+  
+  <div class="portfolio-item-description">
+    <h4><?php print $title; ?></h4>
+    <a class="portfolio-project-button" href="<?php print $node_url;?>">Launch Project</a>
   </div>  
-  <div class="carousel_item_description">
-    <h3><a href="<?php print $node_url;?>"><?php print $title; ?></a></h3>
   </div>  
 </li>
