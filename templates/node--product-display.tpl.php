f7d835ae52bd52b085ac16b330ac583a79f8647c
refaktorco <refaktor.co@gmail.com>
2013-04-30 16:24:51 -0700

    -Tweaking product display node layout.

--- templates/node--product-display.tpl.php
+++ templates/node--product-display.tpl.php
@@ -1,11 +1,20 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  
+  <div class="row">
+    <div class="span5">
 
   <a href="<?php print $node_url; ?>"><?php echo $title; ?></a>
 
   <?php print render($content['product:field_image']); ?>
   
+    </div>
+    <div class="span4">
+  
   <?php print render($content['product:commerce_price']); ?>
   
   <?php print render($content['field_reference']); ?>
+  
+  <?php print render($content); ?>
+    </div>
 
 </article>
